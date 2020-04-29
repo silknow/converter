6659bcbcb9baad38e0b795eb115ec6c3b9eba93b
tschleider <tommy1122000@gmail.com>
2020-04-29 16:20:45 +0200

    Commit for same reason as before

--- src/main/java/org/silknow/converter/converters/MFAConverter.java
+++ src/main/java/org/silknow/converter/converters/MFAConverter.java
@@ -104,7 +104,7 @@ public class MFAConverter extends Converter {
     Transfer transfer = new Transfer(regNum);
     transfer.of(obj).by(museum);
 
-    Collection collection = new Collection(this.DATASET_NAME, regNum);
+    Collection collection = new Collection("MFA", regNum);
     collection.of(obj);
     collection.addAppellation(s.getMulti("Collections").findFirst().orElse(null));
 
