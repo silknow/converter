8b7e60d82505f1d89d0d047257ce139b0470a5ff
tschleider <tommy1122000@gmail.com>
2020-07-22 12:49:42 +0200

    Reconversion

--- src/main/java/org/silknow/converter/entities/Image.java
+++ src/main/java/org/silknow/converter/entities/Image.java
@@ -29,8 +29,8 @@ public class Image extends Entity {
 
   public void generateUri(String id, int imgCount) {
     // this method is called if the img has no identifier and need one uri
-    //String seed = id + "$$$" + imgCount + this.localFilename; // the $$$ is there for avoiding collision of different-sized seeds
-    String seed = MEDIA_BASE;
+    String seed = id + "$$$" + imgCount + this.localFilename; // the $$$ is there for avoiding collision of different-sized seeds
+    //String seed = MEDIA_BASE;
     this.setUri(ConstructURI.build(this.source, "Image", seed));
   }
 
@@ -62,7 +62,7 @@ public class Image extends Entity {
 
       this.addProperty(Schema.contentUrl, model.createResource(internalUrl));
       String seed = internalUrl;
-      this.setUri(ConstructURI.build(this.source, "Image", seed));
+      //this.setUri(ConstructURI.build(this.source, "Image", seed));
     }
 
   }
