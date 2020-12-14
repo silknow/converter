afada1d31436bdcbaa336beb8656cf516f2ffac2
tschleider <tommy1122000@gmail.com>
2020-12-14 22:59:15 +0100

    Forgot to add the new files

---
+++ output/temp.sh
@@ -0,0 +1,8 @@
+
+tar -czf venezia.tar.gz venezia/
+tar -czf paris-musees.tar.gz paris-musees/
+tar -czf mobilier.tar.gz mobilier/
+tar -czf smithsonian.tar.gz smithsonian/
+tar -czf versailles.tar.gz versailles/
+
+
