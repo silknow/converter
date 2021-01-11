cfa7c9428f4046f56e0583a2213e75ea877d3484
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2021-01-11 18:54:07 +0100

    [time parsing] parsing spanish century spans - represent uncertain dates - edtf labels #66

--- src/main/java/org/silknow/converter/entities/Production.java
+++ src/main/java/org/silknow/converter/entities/Production.java
@@ -12,18 +12,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Production extends Entity {
-  private static final String CIRCA_REGEX = "(?i)(circa|around|about|posiblemente|(proba|possi)bly|ca?\\.|\\[ca])";
   private static final String UNCONFIRMED_REGEX = "\\.\\.\\. (unconfirmed|(sin|sense) confirmar)";
   private static final String UNKNOWN_REGEX = "(?i)(de[sc]+ono?cid[oa]\\.?|n\\.d\\.?|no)";
   private static final Pattern UNKNOWN_PATTERN = Pattern.compile(UNKNOWN_REGEX);
-  private static final String ANY_BRACKETS = "[(\\[\\])]";
 
   private int tsCount;
   private boolean timeUnconfirmed;
   private String timeModifier;
-
-  //List<Place> placeList = new ArrayList<>();
-  //public List<Place> getPlaces() {return this.placeList;}
 
   public Production(String id) {
     super(id);
@@ -35,17 +30,17 @@ public class Production extends Entity {
   }
 
   public void addTimeAppellation(String time) {
-    boolean timeApproximate = false;
-
     if (time == null) return;
     time = time.trim();
+
+    System.out.println(time);
 
     Matcher m = UNKNOWN_PATTERN.matcher(time);
     if (m.find()) {
       time = time.replace(m.group(1), "").trim();
-      if (time.matches(ANY_BRACKETS + ".*" + ANY_BRACKETS)) {
-        time = time.replaceAll(ANY_BRACKETS, "").trim();
-      }
+      if (time.matches(TimeSpan.ANY_BRACKETS + ".*" + TimeSpan.ANY_BRACKETS))
+        time = time.replaceAll(TimeSpan.ANY_BRACKETS, "").trim();
+      timeUnconfirmed = true;
     }
     if (time.isEmpty()) return;
 
@@ -53,25 +48,6 @@ public class Production extends Entity {
       timeUnconfirmed = true;
       return;
     }
-    if (time.contains("?"))
-      timeUnconfirmed = true;
-
-    if (time.matches(".*" + CIRCA_REGEX + ".*"))
-      timeApproximate = true;
-
-    // we mostly use replaceAll in order to enable case insensitive (?i) replacements
-    time = time.replaceAll("\\s+", " ");
-    time = time.replaceAll("\\(.*\\)", ""); // curly brackets
-    time = time.replaceAll("\\[.*]", ""); // square brackets
-    time = time.replaceAll(ANY_BRACKETS, ""); // orphan brackets
-
-    time = time.replace("?", "");
-    time = time.replaceAll(CIRCA_REGEX, "");
-    time = time.replaceAll("\"", "");
-    time = time.replaceAll("\\.0$", ""); // workaround some dates as 1960.0
-    time = time.replaceAll("\\.$", ""); // trailing dots
-    time = time.trim();
-    if (time.isEmpty()) return;
 
     for (String regex : TimeSpan.CENTURY_PART_REGEXES) { // case "last quarter"
       if (time.matches(regex)) {
@@ -82,10 +58,7 @@ public class Production extends Entity {
     }
 
     TimeSpan ts = new TimeSpan(timeModifier + time);
-    // centralising the ts definition, those values are incorrect
-    // if (timeUnconfirmed) ts.addNote("unconfirmed", "en");
-    // if (timeApproximate) ts.addNote("approximate", "en");
-
+    ts.setUncertain(timeUnconfirmed);
     this.addTimeSpan(ts);
     tsCount++;
   }
